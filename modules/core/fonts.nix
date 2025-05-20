c28261cf8e9166d8faad75788032ba4b4d566f2a
shao-rj-jack <shao.rj.jack@gmail.com>
2025-05-20 19:38:26 -0400

    adding p10k config to zsh and some fonts

---
+++ modules/core/fonts.nix
@@ -0,0 +1,15 @@
+{pkgs, ...}:
+
+{
+  fonts = {
+    packages = with pkgs; [
+      fira-code
+      fira-code-symbols
+      font-awesome
+      material-icons
+      noto-fonts-cjk-sans
+      noto-fonts-emoji
+      symbola
+    ];
+  };
+}
--- modules/home/zsh/default.nix
+++ modules/home/zsh/default.nix
@@ -1,11 +1,33 @@
+{pkgs, lib, ...}:
+
 {
   programs.zsh = {
     enable = true;
     enableCompletion = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
+
     oh-my-zsh = {
       enable = true;
+    };
+
+    plugins = [
+      {
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+      {
+        name = "powerlevel10k-config";
+        src = lib.cleanSource ./p10k-config;
+        file = "p10k.zsh";
+      }
+    ];
+
+    shellAliases = {
+      v = "vim";
+      os-home = "cd ~/riceOS";
+      os-rebuild = "cd ~/riceOS; sudo nixos-rebuild switch --flake \'.#riceOS\'";
     };
   };
 }
