49b1ec357b749726ba9c51843bae5b44c7293c5b
shao-rj-jack <shao.rj.jack@gmail.com>
2025-05-20 21:45:43 -0400

    adding kitty.nix

--- modules/home/zsh/default.nix
+++ modules/home/zsh/default.nix
@@ -31,7 +31,8 @@
     shellAliases = {
       v = "vim";
       os-home = "cd ~/riceOS";
-      os-rebuild = "cd ~/riceOS; sudo nixos-rebuild switch --flake \'.#riceOS\'";
+      os-rebuild = "cd ~/riceOS && sudo nixos-rebuild switch --flake \'.#riceOS\'";
+      os-ncg = "nix-collect-garbage --delete-old && sudo nix-collect-garbage -d && sudo /run/current-system/bin/switch-to-configuration boot";
     };
   };
 }
