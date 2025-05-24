a03e897e3d83ff88d46d1365bf0d2351cfa134da
shao-rj-jack <shao.rj.jack@gmail.com>
2025-05-24 14:55:40 -0400

    base hyprland configs

--- modules/home/hyprland/default.nix
+++ modules/home/hyprland/default.nix
@@ -11,20 +11,142 @@
 
     settings = {
       general = {
-        "$modifier" = "SUPER";
+        layout = "dwindle";
+        gaps_in = 6;
+        gaps_out = 8;
+        border_size = 2;
+        resize_on_border = true;
+        # "col.active_border"
+        # "col.inactive_border"
       };
+
+      dwindle = {
+        pseudotile = true;
+        preserve_split = true;
+        force_split = 2;
+      };
+
+      decoration = {
+        rounding = 10;
+
+        blur = {
+          enabled = true;
+          size = 5;
+          passes = 3;
+          ignore_opacity = false;
+          new_optimizations = true;
+        };
+
+        shadow = {
+          enabled = true;
+          range = 4;
+          render_power = 3;
+          color = "rgba(1a1a1aee)";
+        };
+      };
+
+      animations = {
+        enabled = true;
+
+        bezier = [
+          "wind, 0.05, 0.9, 0.1, 1.05"
+          "winIn, 0.1, 1.1, 0.1, 1.1"
+          "winOut, 0.3, -0.3, 0, 1"
+          "liner, 1, 1, 1, 1"
+        ];
+
+        animation = [
+          "windows, 1, 6, wind, slide"
+          "windowsIn, 1, 6, winIn, slide"
+          "windowsOut, 1, 5, winOut, slide"
+          "windowsMove, 1, 5, wind, slide"
+          "border, 1, 1, liner"
+          "borderangle, 1, 30, liner, loop"
+          "fade, 1, 10, default"
+          "workspaces, 1, 5, wind"
+        ];
+      };
+
+      master = {
+        new_status = "master";
+        new_on_top = 1;
+        mfact = 0.5;
+      };
+
+      misc = {
+        layers_hog_keyboard_focus = true;
+        initial_workspace_tracking = 0;
+        mouse_move_enables_dpms = true;
+        key_press_enables_dpms = false;
+        disable_hyprland_logo = true;
+        disable_splash_rendering = true;
+        enable_swallow = false;
+        vfr = true;
+        vrr = 2;
+        enable_anr_dialog = true;
+        anr_missed_pings = 20;
+      };
+
+      gestures = {
+        workspace_swipe = 1;
+        workspace_swipe_fingers = 3;
+        workspace_swipe_distance = 500;
+        workspace_swipe_invert = 1;
+        workspace_swipe_min_speed_to_force = 30;
+        workspace_swipe_cancel_ratio = 0.5;
+        workspace_swipe_create_new = 1;
+        workspace_swipe_forever = 1;
+      };
+
+      ecosystem = {
+        no_donation_nag = true;
+        no_update_news = false;
+      };
+
+      cursor = {
+        sync_gsettings_theme = true;
+        no_hardware_cursors = 2;
+        enable_hyprcursor = false;
+        warp_on_change_workspace = 2;
+        no_warps = true;
+      };
+
+      render = {
+        explicit_sync = 1;
+        explicit_sync_kms = 1;
+        direct_scanout = 0;
+      };
+
       input = {
         kb_layout = "us";
+        numlock_by_default = true;
+        repeat_delay = 300;
+        follow_mouse = 1;
+        float_switch_override_focus = 0;
+        sensitivity = 0;
+
         touchpad = {
           natural_scroll = true;
           disable_while_typing = true;
           scroll_factor = 0.8;
         };
       };
+
+      env = [
+        "NIXOS_OZONE_WL, 1"
+        "NIXPKGS_ALLOW_UNFREE, 1"
+        "EDITOR, nvim"
+      ];
+
+      "$modifier" = "SUPER";
       bind = [
         "$modifier,Return,exec,kitty"
         "$modifier,W,exec,firefox"
       ];
     };
+
+    extraConfig = "
+      monitor = ,preferred,auto,auto
+    ";
   };
 }
