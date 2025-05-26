40de141c5eea968fe0fe29b5b1458c5c6567cf33
shao-rj-jack <shao.rj.jack@gmail.com>
2025-05-26 13:58:35 -0400

    switch to oh-my-posh for zsh

--- modules/home/zsh/default.nix
+++ modules/home/zsh/default.nix
@@ -1,32 +1,12 @@
-{
-  pkgs,
-  lib,
-  ...
-}: {
+{pkgs, ...}: {
   programs.zsh = {
     enable = true;
     enableCompletion = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
 
-    oh-my-zsh = {
-      enable = true;
-    };
-
-    plugins = [
-      {
-        name = "powerlevel10k";
-        src = pkgs.zsh-powerlevel10k;
-        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-      }
-      {
-        name = "powerlevel10k-config";
-        src = lib.cleanSource ./p10k-config;
-        file = "p10k.zsh";
-      }
-    ];
-
     initContent = ''
+      eval "$(${pkgs.oh-my-posh}/bin/oh-my-posh init zsh --config ${./oh-my-posh.toml})"
       fastfetch
     '';
 
